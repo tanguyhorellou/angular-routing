331d238b313a6d06c72990bd39d33c215832a275
tanguyhorellou <40953050+tanguyhorellou@users.noreply.github.com>
2019-09-20 15:25:32 +0200

    last update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { TermsComponent } from './terms/terms.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ArticlesModule } from './articles/articles.module';
+import { ProductsModule } from './products/products.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -16,20 +20,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     AboutComponent,
     PrivacyComponent,
-    TermsComponent
+    TermsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    ArticlesModule,
+    ProductsModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-
-  imports: [
-  BrowserModule,
-  NgbModule,
-  ArticlesModule,
-  ProductsModule,
-  AppRoutingModule,
-  BrowserModule,
-  BrowserAnimationsModule,
-],
-
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
